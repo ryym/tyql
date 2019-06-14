58b18bfd3040467f59b2d33c6cf452e2db528c91
ryym <ryym.64@gmail.com>
2019-06-15 08:19:32 +0900

    refactor: Remove v0.1 code

--- v0.1/tyql.ts
+++
@@ -1,1 +0,0 @@
-export { table, to } from './table';
--- v0.1/util.ts
+++
@@ -1,5 +0,0 @@
-// https://patrickdesjardins.com/blog/typescript-exhaustive-check
-// You can use this to do exhaustive check in compile time.
-export const unreachable = (_value: never): never => {
-  throw new Error('unreachable');
-};
