04257efbd81023dbd2e26c524f8264dc9932dbd5
ryym <ryym.64@gmail.com>
2019-06-19 08:00:03 +0900

    feat: Add From type to query builder
    builder.innerJoin only accepts joins between From and other type

--- src/queryBuilder.ts
+++ src/queryBuilder.ts
@@ -18,17 +18,19 @@ const unimplemented = (): never => {
   throw new Error('unimplemented');
 };
 
-export class QueryBuilder<R, Ms> {
+export class QueryBuilder<From, R, Ms> {
   constructor(private readonly query: Query<Ms>) {}
 
-  select<Sels extends Selectable<Ms>[]>(...sels: Sels): QueryBuilder<Select<ValuesOf<Sels>>, Ms> {
-    return new QueryBuilder<Select<ValuesOf<Sels>>, Ms>({
+  select<Sels extends Selectable<Ms>[]>(
+    ...sels: Sels
+  ): QueryBuilder<From, Select<ValuesOf<Sels>>, Ms> {
+    return new QueryBuilder<From, Select<ValuesOf<Sels>>, Ms>({
       ...this.query,
       select: sels,
     });
   }
 
-  innerJoin<M1 extends Ms, M2>(joiner: Joiner<M1, M2>): QueryBuilder<AddColumn<R, M2>, Ms | M2> {
+  innerJoin<M2>(joiner: Joiner<From, M2>): QueryBuilder<From, AddColumn<R, M2>, Ms | M2> {
     const join = joiner();
     const query: Query<Ms | M2> = { ...this.query };
     query.defaultSelect.push(join.rightColumns());
@@ -36,32 +38,32 @@ export class QueryBuilder<R, Ms> {
     return new QueryBuilder(query);
   }
 
-  where(...preds: IExpr<boolean, Ms>[]): QueryBuilder<R, Ms> {
+  where(...preds: IExpr<boolean, Ms>[]): QueryBuilder<From, R, Ms> {
     this.query.where = this.query.where.concat(preds);
     return this;
   }
 
-  groupBy(...exprs: Groupable<Ms>[]): QueryBuilder<R, Ms> {
+  groupBy(...exprs: Groupable<Ms>[]): QueryBuilder<From, R, Ms> {
     this.query.groupBy = this.query.groupBy.concat(exprs);
     return this;
   }
 
-  having(...preds: IExpr<boolean, Ms>[]): QueryBuilder<R, Ms> {
+  having(...preds: IExpr<boolean, Ms>[]): QueryBuilder<From, R, Ms> {
     this.query.having = this.query.having.concat(preds);
     return this;
   }
 
-  orderBy(...ords: Orderer<Ms>[]): QueryBuilder<R, Ms> {
+  orderBy(...ords: Orderer<Ms>[]): QueryBuilder<From, R, Ms> {
     this.query.orderBy = this.query.orderBy.concat(ords);
     return this;
   }
 
-  limit(n: number): QueryBuilder<R, Ms> {
+  limit(n: number): QueryBuilder<From, R, Ms> {
     this.query.limit = n;
     return this;
   }
 
-  offset(n: number): QueryBuilder<R, Ms> {
+  offset(n: number): QueryBuilder<From, R, Ms> {
     this.query.offset = n;
     return this;
   }
